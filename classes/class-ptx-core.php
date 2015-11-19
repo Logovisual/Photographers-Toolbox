d5a345ceb502092f6473ff463c405eeb79442c5a
Kenth Hagström <info@kenthhagstrom.se>
2015-11-19 23:55:23 +0100

    Added paypal donation link
    Added PayPal donation link to the plugin meta in the plugins.php page.

--- classes/class-ptx-core.php
+++ classes/class-ptx-core.php
@@ -42,8 +42,23 @@ class PTX_Core extends PTX_Shared {
 
 		$plugin_basename = plugin_basename( dirname( dirname( __FILE__ ) ).'/ptx.php' );
 		add_filter( "plugin_action_links_$plugin_basename", array( $this, 'add_plugin_settings_link' ) );
+		add_filter( 'plugin_row_meta', array( $this, 'add_plugin_meta' ), 10, 2 );
 	}
 
+	function add_plugin_meta( $links, $file ) {
+
+		if ( strpos( $file, 'ptx.php' ) !== false ) {
+			$new_links = array(
+				'<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=info%40kenthhagstrom%2ese&lc=SE&item_name=Photographers%20Toolbox%20WordPress%20Plugin&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted">'.__( 'Donate', $this->domain ) . '</a>'
+			);
+			$links = array_merge( $links, $new_links );
+		}
+		return $links;
+	}
+
+	/**
+	 * Add settings link to plugin
+	 */
 	function add_plugin_settings_link( $links ) {
 		$settings_link = '<a href="admin.php?page=ptx_menu_page">' . __( 'Settings', $this->domain ) . '</a>';
 		$links[] = $settings_link;
