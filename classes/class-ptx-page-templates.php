ae3892afea8b6300b53e5d185233f96064c78ec2
Kenth Hagström <info@kenthhagstrom.se>
2015-11-19 22:36:12 +0100

    Added page templates object

---
+++ classes/class-ptx-page-templates.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Page Templates
+ *
+ * Include page templates to enable easy theming.
+ *
+ * @package Photographers Toolbox
+ * @subpackage Classes
+ * @since 0.1.0
+ */
+
+// Direct access not allowed.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Page Templates
+ *
+ * Include page templates to enable easy theming.
+ *
+ * @since 0.1.0
+ */
+class PTX_Page_Templates extends PTX_Shared {
+
+}
